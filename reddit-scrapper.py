9c353bf06574e8d8657c6ef65e0fca799eb071b0
Thushan Weerarathne <42800067+wthushan01@users.noreply.github.com>
2019-10-15 08:39:04 +0530

    Updated for v1.1 (still work in progress)

--- reddit-scrapper.py
+++ reddit-scrapper.py
@@ -1,45 +1,62 @@
-import time
-import sys
 from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as urlRequest
-from urllib.request import urlretrieve as urlRetrieve
+from urllib.request import urlopen as url_request
+from urllib.request import urlretrieve as url_retrieve
+import re
 
-# this could be changed to any url according to user's liking
-page_url = 'https://old.reddit.com/r/witcher'
+# getting user inputs
+url_input = input("Enter the  URL: http://www.reddit.com/r/")
+page_url = 'https://old.reddit.com/r/' + str(url_input)
+number_of_attempts = input("Enter the number of retry attempts: ")
 
-try:
-    urlClient = urlRequest(page_url)
-except Exception:
-    # for loop is used instead of time.sleep(5) to make the countdown timer
-    for i in range(5):
-        # most of the time the error is a 429 error. best solution is to try again after few seconds
-        msg = str('Error occurred. Retrying in ' + str(5-i) + ' secs.')
-        # \r is used to move cursor back to the beginning of the line so that it won't print on a new line
-        sys.stdout.write('\r' + msg)
-        time.sleep(1)
-    # trying for the second time. this could be improved using a loop. will check later
+for i in range(int(number_of_attempts)):
     try:
-        urlClient = urlRequest(page_url)
+        url_client = url_request(page_url)
+        if url_client != -1:
+            break
     except Exception:
-        # if error is still there then display a message and terminate the process
-        sys.stdout.write('\nRetrying failed. Please check your internet connection and run again.')
-        exit()
+        print("Error occurred. Retrying")
 
 # parsing the html
-page_soup = soup(urlClient.read(), "html.parser")
+page_soup = soup(url_client.read(), "html.parser")
 # close the open internet connection
-urlClient.close()
+url_client.close()
 
 # getting image urls to img_urls list
 img_urls = []
+post_urls = []
+post_names = []
+file_types = []
+
 containers = page_soup.findAll("div", {"data-type": "link"})
+name_containers = page_soup.findAll("a", {"data-event-action": "title"})
+
+# finding image urls
 for container in containers:
     urls = container["data-url"]
     # finding urls with .jpg at the end because some reddit posts may not have image on them thus causing an error
     if urls.find(".jpg") != -1:
         img_urls.append(urls)
+        post_urls.append(container["data-permalink"])
+        file_types.append(".jpg")
+    if urls.find(".png") != -1:
+        img_urls.append(urls)
+        post_urls.append(container["data-permalink"])
+        file_types.append(".png")
+
+# finding image names
+for each_url in post_urls:
+    for each_name in name_containers:
+        if each_url == each_name["href"]:
+            # removing special characters and setting the length of file names
+            name = re.sub('[^A-Za-z0-9 ]+', '', each_name.text)
+            name = name[:250]
+            post_names.append(name)
+            break
+
+# print the number of images detected
+print(len(img_urls), "images have been detected")
 
 # downloading the image
 for i in range (len(img_urls)):
-    file_name = str(i) + '.jpg'
-    urlRetrieve(img_urls[i], file_name)
+    file_name = str(post_names[i]) + str(file_types[i])
+    url_retrieve(img_urls[i], file_name)
